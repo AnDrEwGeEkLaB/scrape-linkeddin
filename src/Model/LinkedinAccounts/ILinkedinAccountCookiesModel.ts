efe48c938e2bc6669eca0f49d9e495e81a46cd3c
AnDrEwGeEkLaB <andrewgeeklab@gmail.com>
2024-10-01 13:51:34 +0300

    delete all

--- src/Model/LinkedinAccounts/ILinkedinAccountCookiesModel.ts
+++
@@ -1,8 +0,0 @@
-import { Types } from "mongoose";
-import { Cookie } from "puppeteer";
-
-export default interface ILinkedinAccountCookiesModel {
-    cookies: Cookie[];
-    email: string;
-    password: string;
-}
--- src/Service/LinkedinAccountCookies/ILinkedinAccountCookiesService.ts
+++
@@ -1,8 +0,0 @@
-import { Cookie } from "puppeteer";
-import ILinkedinAccountCookiesModel from "../../Model/LinkedinAccounts/ILinkedinAccountCookiesModel";
-
-export default interface ILinkedinAccountCookiesService {
-    saveCookies(cookies: ILinkedinAccountCookiesModel): Promise<ILinkedinAccountCookiesModel>;
-    getCookies(_id: string): Promise<ILinkedinAccountCookiesModel | null>;
-    updateCookies(_id: string, cookies: Cookie[]): Promise<ILinkedinAccountCookiesModel | null>;
-}
