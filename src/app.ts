624efafe94887bcdbff3196ad79a0ba034bf727a
GeekLabsHoldings <129297744+GeekLabsHoldings@users.noreply.github.com>
2024-11-28 15:30:06 +0200

    updates

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,9 @@
 import express, { Application } from "express";
 import cors from "cors";
-import WebSocket, { WebSocketServer } from 'ws';
 
 import LinkedinRouter from "./Router/LinkedinRouter";
-import { clients, initializeClient } from "./Controller/Whatsapp/WhatsappController";
-import WhatsappRouter from "./Router/WhatsappRouter";
 const app: Application = express();
 
-const wss = new WebSocketServer({ noServer: true });
-wss.on('connection', (ws: WebSocket) => {
-    const clientId = '1';
-    initializeClient(clientId);
-    setTimeout(() => {
-        console.log("Init client")
-        console.log(`WebSocket connection established for client ${clientId}`);
-        clients[clientId].client.on('message', async (msg: { from: string; body: string }) => {
-            ws.send(JSON.stringify({ from: msg.from, body: msg.body }));
-        });
-        clients[clientId].client.on('qr', (qr: string) => {
-            ws.send(JSON.stringify({ body: qr }));
-        });
-        clients[clientId].client.on('ready', (qr: string) => {
-            ws.send(JSON.stringify({ body: 'Client Is Ready' }));
-        })
-    }, 2000);
-})
 
 app.use(cors());
 
@@ -35,7 +14,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/linkedin", LinkedinRouter);
-app.use('/whats-app', WhatsappRouter)
 
 
-export { app, wss };
+export { app};
