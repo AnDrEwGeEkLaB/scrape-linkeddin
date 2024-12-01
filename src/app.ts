8274e226e8e50598fe1442a2176d5090388e0333
GeekLabsHoldings <129297744+GeekLabsHoldings@users.noreply.github.com>
2024-12-01 09:39:44 +0200

    remove delay

--- src/app.ts
+++ src/app.ts
@@ -9,13 +9,8 @@ app.use(cors());
 
 app.use(express.json());
 
-const delay = async (time: number) => {
-    return new Promise(function (resolve) {
-        setTimeout(resolve, time)
-    });
-}
+
 app.get("/", async (req, res) => {
-    await delay(3 * 60 * 1000);
     res.send("Hello World");
 });
 
